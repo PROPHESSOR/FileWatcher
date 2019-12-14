15c720d59aaf3009ae425341adc5eef044decf28
PROPHESSOR <xenteze@gmail.com>
2019-12-14 20:56:04 +0200

    Максимально уменьшен размер приложения

--- app/src/main/java/com/prophessor/filewatcherphg/MainActivity.java
+++ app/src/main/java/com/prophessor/filewatcherphg/MainActivity.java
@@ -3,14 +3,10 @@ package com.prophessor.filewatcherphg;
 import android.os.Bundle;
 import org.apache.cordova.CordovaActivity;
 
-public class MainActivity extends CordovaActivity
-{
-
+public class MainActivity extends CordovaActivity {
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Set by <content src="index.html" /> in config.xml
         loadUrl(launchUrl);
     }
 }
