fd481f740c6a62bf51405802280be13750275db2
PROPHESSOR <xenteze@gmail.com>
2019-12-14 20:08:51 +0200

    Исправлен код, чтобы он действительно работал

--- app/src/main/java/com/prophessor/filewatcher/MainActivity.java
+++ app/src/main/java/com/prophessor/filewatcher/MainActivity.java
@@ -2,22 +2,23 @@ package com.prophessor.filewatcher;
 
 import android.app.*;
 import android.os.*;
+import android.widget.*;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
+		
         ListView snapshotList = (ListView) findViewById(R.id.snapshotList);
 
-        String[] list = {};
+        String[] list = new String[100];
 
         for (int i = 0; i < 100; i++) {
-            list.push("Item" + i);
+            list[i] = "Item" + i;
         }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list)
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
 
         snapshotList.setAdapter(adapter);
     }
